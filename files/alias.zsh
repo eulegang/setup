0c7e1620a9df5217a6d37fb997232dd394be5a67
eulegang <eulegang@eulegang.dev>
2024-03-09 22:06:18 -0600

    Adding basic zsh configs

---
+++ files/alias.zsh
@@ -0,0 +1,13 @@
+
+alias vi=nvim
+alias vim=nvim
+
+alias gst='git status'
+alias gc='git commit'
+alias gc!='git commit --amend'
+alias gb='git branch'
+alias ga='git add'
+alias gaa='git add -a'
+alias gd='git diff'
+
+
--- init.zsh
+++ init.zsh
@@ -1,6 +1,11 @@
 #! /bin/zsh
 
+echo "installing i3 config"
 cp files/i3.conf ~/.config/i3/config
+
+echo "installing zsh config"
+cp files/zshrc ~/.zshrc
+cp files/alias.zsh ~/.alias.zsh
 
 
 
